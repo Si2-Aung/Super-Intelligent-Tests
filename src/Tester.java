ae9d4e1a319526795dc5ce1c1b1e0fabeabd8da8
Si2-Aung <C2aung7@gmail.com>
2022-10-29 14:53:09 +0200

    BubbleSort added

--- src/Tester.java
+++ src/Tester.java
@@ -1,11 +1,19 @@
 public class Tester {
 
-    public int add(int a, int b){
-        return a+b;
-    }
-
-    public double add( double b, double c){
-        return b+c;
+    public static int[] bubblesort(int[] toSort) {
+        int [] result = toSort;
+        for (int i = 0; i < result.length-1; i++) {
+            for (int j = 0; j < result.length-i-1; j++) {
+                int k = j+1;
+                if(result[j] > result[k]) {
+                    int tmp = result[j];
+                    result[j] = result[k];
+                    result[k] = tmp;
+                    k++;
+                }
+            }
+        }
+        return result;
     }
 
 }
