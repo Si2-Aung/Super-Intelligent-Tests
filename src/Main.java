414639b4749b066ddc3fa8bebf12888f97fb6016
Si2-Aung <C2aung7@gmail.com>
2022-11-02 10:24:13 +0100

    BubbleSort added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("No World");
+        String [] teste = new String[]{"indis0","indis1","indis2","indis3","indis4","indis5","indis6","indis7","indis8",null,"indis10","indis11"};
+        int [] indis = {1,3,9};
+        sorting(teste,indis);
+
+        for (String a: teste) {
+            System.out.println(a);
+        }
+    }
+
+    public static void sorting (String [] teste, int[]indis){
+        int i = indis[0];
+        int j = indis[1];
+        int k = indis[2];
+
+        String tmp = teste[i];
+
+        teste[i] = teste[j];
+        teste[j] =teste[k];
+        teste[k] = tmp;
     }
 }
